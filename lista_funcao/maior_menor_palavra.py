7dbb3e342969d7bd5dadde9092649fdddc24ba86
Josyani Santos <94111206+AniSnt@users.noreply.github.com>
2025-04-04 22:38:53 -0300

    Update maior_menor_palavra.py

--- lista_funcao/maior_menor_palavra.py
+++ lista_funcao/maior_menor_palavra.py
@@ -1,24 +1,31 @@
- # lista de palavras 
+# lista de palavras 
 palavras = ["casa", "flamengo", "paralelepipedo", "computador", "livro", "caneta"]
 
-def maiorPalavra(lista_palavras):
-    palavra_mais_longa = lista_palavras
+def maiorPalavra(lista_palavras: list):
 
+    if len(lista_palavras) == 0:
+        return print("a lista esta vazia ")
+
+    palavra_mais_longa = ""
     for palavra in lista_palavras:
-        if len(palavra) > len(palavra_mais_longa):
+        if palavra > palavra_mais_longa:
             palavra_mais_longa = palavra
 
     return palavra_mais_longa
     
-def menorPalavra(lista_palavras):
-    palavra_mais_curta = lista_palavras
-
+def menorPalavra(lista_palavras: list):
+    
+    if len(lista_palavras) == 0:
+        return print("a lista esta vazia, não podemos fazer nada por você")
+    
+    palavra_mais_curta = lista_palavras[0]
     for palavra in lista_palavras:
         if len(palavra) < len(palavra_mais_curta):
             palavra_mais_curta = palavra
-                
+            
     return palavra_mais_curta
             
 print(f"Palavra mais longa da lista: {maiorPalavra(palavras)}")
 print(f"Palavra mais curta da lista: {menorPalavra(palavras)}")
+ 
  
